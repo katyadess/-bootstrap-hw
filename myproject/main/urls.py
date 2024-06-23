6af72ac6fb6572ad6ab455a78d10d56ad017d41d
katyadess <trynovakatya345@gmail.com>
2024-06-23 21:44:15 +0300

    modified pages and added some links

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('demo/', views.demo, name='demo'),
+    path('archives/', views.archives, name='archives'),
+    path('blog/', views.blog, name='blog'),
+    path('page/', views.page, name='page'),
+    path('single/', views.single, name='single')
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def archives(request):
+    return render(request, 'main/archives.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def demo(request):
+    return render(request, 'main/demo.html')
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def page(request):
+    return render(request, 'main/page.html')
+
+
+def single(request):
+    return render(request, 'main/single.html')
